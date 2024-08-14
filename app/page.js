3d6b8f83470e44b78c7e7eee73de7082eb684ce4
loisemusungu <mwongelimusungu@gmail.com>
2024-08-14 12:43:46 +0300

    Creating Flashcard SaaS with NextJS

---
+++ app/page.js
@@ -0,0 +1,112 @@
+import Image from "next/image";
+import getStripe from "../utils/get-stripe.js";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+  AppBar,
+  Box,
+  Grid,
+} from "@mui/material";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <Container maxwidth="100vw">
+      <Head>
+        <title>Flashcard Saas</title>
+        <meta name="description" content="Create Flashcard from your text" />
+      </Head>
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Flashcard Saas
+          </Typography>
+          <SignedOut>
+            <Button color="inherit"> Login</Button>
+            <Button color="inherit">Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+      <Box
+        sx={{
+          textAlign: "center",
+          my: 4,
+        }}
+      >
+        <Typography variant="h2">Welcome to Flashcard SaaS</Typography>
+        <Typography variant="h5">
+          {" "}
+          The easiest way to make flashcards from your text
+        </Typography>
+        <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+          Get Started
+        </Button>
+      </Box>
+      <Box sx={{ my: 6 }}>
+        <Typography variant="h4">Features</Typography>
+        <Grid contained spacing={4}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Easy Text Input</Typography>
+            <Typography>
+              {" "}
+              Simply input your text and let our software do the rest. Creating
+              flashcards has never been easier!
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Smart Flashcards</Typography>
+            <Typography>
+              {" "}
+              Our AI intelligently breaks down your text into concise
+              flashcards, perfect for studying.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Accessible Anywhere</Typography>
+            <Typography>
+              {" "}
+              Access your flashcards from any device, at anytime. Study on the
+              go with ease.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ my: 6, textAlign: "center" }}>
+        <Typography variant="h4">Pricing</Typography>
+        <Grid contained spacing={4}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Easy Text Input</Typography>
+            <Typography>
+              {" "}
+              Simply input your text and let our software do the rest. Creating
+              flashcards has never been easier!
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Smart Flashcards</Typography>
+            <Typography>
+              {" "}
+              Our AI intelligently breaks down your text into concise
+              flashcards, perfect for studying.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Accessible Anywhere</Typography>
+            <Typography>
+              {" "}
+              Access your flashcards from any device, at anytime. Study on the
+              go with ease.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWeAcyu6gNGUacEPE--OY--edqMgL9K9I",
+  authDomain: "lm-flashcardsaas.firebaseapp.com",
+  projectId: "lm-flashcardsaas",
+  storageBucket: "lm-flashcardsaas.appspot.com",
+  messagingSenderId: "369342105499",
+  appId: "1:369342105499:web:94037d0bcae21ebb30033e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
